2f03d584b231457f3b96b28c3fe00df168de5615
Anshcodeify45 <anishpatnaik45@gmail.com>
2025-01-18 14:30:31 +0530

    day-1 test-3

--- dashboard/src/Component/Home.jsx
+++ dashboard/src/Component/Home.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Box} from '@mui/material'
+import { Box,Typography} from '@mui/material'
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+
 function Home() {
   return (
-    <Box style={{marginTop:34}}>
+    <Box style={{marginTop:78}}>
     <Navbar/>
-    <Box>
-    <h1>Home</h1>
+    <Box style={{marginLeft:10}}>
+    <Grid container spacing={2}>
+        <Grid item xs={8}>
+        <Stack spacing={2} direction="row">
+        <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+          
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+    </Stack>
+        </Grid>
+        <Grid item xs={4}>
+         
+        </Grid>
+      </Grid>
     </Box>
     </Box>
   )
