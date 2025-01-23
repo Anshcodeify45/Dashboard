994d25079ce9e4ec700f0a940dc6b4eadb3ec625
Anshcodeify45 <anishpatnaik45@gmail.com>
2025-01-23 23:56:07 +0530

    day3

--- dashboard/src/Component/Home.jsx
+++ dashboard/src/Component/Home.jsx
@@ -1,42 +1,74 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Box,Typography} from '@mui/material'
+import { Box,Typography,styled} from '@mui/material'
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import Accordionn from './Accordionn';
+
+
+
+
+
+const ParentBox = styled(Box)`
+ display:flex;
+ align-items:center;
+ justify-content:space-evenly;
+ width:70%;
+`
+
+const Leftcard = styled(Box)`
+ width:100%; 
+`
 
 function Home() {
   return (
     <Box style={{marginTop:78,marginLeft:10,border:"2px solid",width:"100%",height:"88vh"}}>
     <Navbar/>
     <Box>
-    <Grid container spacing={2} style={{paddingTop:25}}>
+    <Grid container spacing={2} style={{paddingTop:25,paddingLeft:10}}>
         <Grid item xs={8}>
         <Stack spacing={2} direction="row">
-        <Card sx={{ maxWidth: 49 +"%" ,height:140 }}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        <Card sx={{ width: 99 +"%" ,height:130}}>
+      <CardContent style={{backgroundColor:"#2459ab",color:"white"}}>
+        <Leftcard >
+          <Box><LocalAtmIcon/></Box>
+          <Box>
+          <Typography gutterBottom variant="h5" component="div">
+            $500.00
+          </Typography>
+          </Box>
+          <Box>
+          <Typography variant="body2">
+            Total Earnings
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          </Box>
+        </Leftcard>
+      </CardContent>
+    </Card>
+
+    <Card sx={{ width: 99 +"%" ,height:130}}>
+      <CardContent style={{backgroundColor:"#2459ab",color:"white"}}>
+        <Leftcard >
+          <Box><LocalAtmIcon/></Box>
+          <Box>
+          <Typography gutterBottom variant="h5" component="div">
+            $500.00
+          </Typography>
+          </Box>
+          <Box>
+          <Typography variant="body2">
+            Total Earnings
         </Typography>
+          </Box>
+        </Leftcard>
       </CardContent>
     </Card>
           
-    <Card sx={{ maxWidth: 49 +"%" ,height:140 }}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-    </Card>
+   
     </Stack>
         </Grid>
 
@@ -44,14 +76,27 @@ function Home() {
         <Grid item xs={4}>
         <Stack spacing={2}>
         <Card sx={{ maxWidth: 345 }}>
-      <CardContent>
+      <CardContent style={{backgroundColor:" #3d99b8",color:"white"}}>
+        <ParentBox>
+          <Box><AccountBalanceIcon/></Box>
+          <Box>
+            <Box><Typography style={{fontWeight:600}}>$203K</Typography></Box>
+            <Box><Typography>Total Income</Typography></Box>
+          </Box>
+        </ParentBox>
        
       </CardContent>
     </Card>
           
     <Card sx={{ maxWidth: 345 }}>
-      <CardContent>
-        
+      <CardContent style={{backgroundColor:" #3d99b8",color:"white"}}>
+      <ParentBox>
+          <Box><AccountBalanceIcon/></Box>
+          <Box>
+            <Box><Typography style={{fontWeight:600}}>$203K</Typography></Box>
+            <Box><Typography>Total Income</Typography></Box>
+          </Box>
+        </ParentBox>
       </CardContent>
     </Card>
     </Stack>
@@ -61,9 +106,9 @@ function Home() {
       <Grid container spacing={2} style={{paddingTop:25}}>
         <Grid item xs={8}>
         <Stack spacing={2}>
-        <Card sx={{ height:60 + "vh",border:"2px solid"}}>
+        <Card sx={{ height:50 + "vh",border:"2px solid"}}>
       <CardContent>
-
+        
       </CardContent>
     </Card>
         
@@ -73,11 +118,20 @@ function Home() {
         
         <Grid item xs={4}>
         
-        <Card sx={{ height:60 + "vh" ,border:"2px solid"}}>
+        <Card sx={{ height:50 + "vh" ,border:"2px solid"}}>
       <CardContent>
+        <Box>
+        <Box>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          Popular Products
         </Typography>
+        </Box>
+        <Box>
+        <Accordionn/>
+        </Box>
+        </Box>
+        
+        
       </CardContent>
     </Card>
     
